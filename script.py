d456b913bb5ae6f9b93a93ed22f0f267d310e518
Dkingofcode <doladepo128@gmail.com>
2023-01-17 10:27:06 +0000

    Finished program

---
+++ script.py
@@ -0,0 +1,44 @@
+def replace_word():
+
+       str = "hi guys, i am david, and hi"
+       word_to_replace  = input("Enter the word to replace: ")
+       word_replacement = input("Enter the word replacement: ")
+       print(str.replace(word_to_replace, word_replacement))
+
+replace_word()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
